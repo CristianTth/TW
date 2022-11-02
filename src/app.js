94eb66ceefe895c0936f4a7676e4d2da3c25e672
CristianTth <cristian.toth01@e-uvt.ro>
2022-11-02 22:16:25 +0200

    Lab4

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+const container = document.getElementById("container");
+
+function renderProduct(title, description, price, imageURL) {
+    return `<div class="product">
+              <img class="image" src="${imageURL}" />
+              <span class="title">${title}</span>
+              <span class="description">${description}</span>
+              <span class="price">${price}$</span>
+           </div>
+      `;
+}
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(json => {
+    for (const product of json.products) {
+        let productElement = document.createElement('div');
+        productElement.innerHTML = renderProduct(
+            product.title, 
+            product.description,
+            product.price,
+            product.thumbnail
+        );
+        container.appendChild(productElement);
+    }
+})
